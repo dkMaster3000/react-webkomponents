f0713e955c48d60292c80ce0d3b6f0e800984d84
Denis Kunz <denis.kunz@INFO.TIME-FOR-KIDS>
2021-10-25 13:05:20 +0200

    changed packege&app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactWebComponent from "react-web-component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
 }
 
-export default App;
+class AnotherComponent extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}!</div>;
+  }
+}
+
+ReactWebComponent.create(<MyComponent />, "my-component");
+//ReactWebComponent.create(<AnotherComponent name="Mars" />, 'another-component');
